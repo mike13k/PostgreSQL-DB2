08e4b3a2fb4f300b190916c2c102a368f67d36d3
Basem Rizk <basem.rizk@outlook.com>
2018-04-13 15:28:03 +0200

    All kind of insertions written and commented

--- src/PostgreSQLJDBC.java
+++ src/PostgreSQLJDBC.java
@@ -6,16 +6,19 @@ import java.sql.DriverManager;
 
 public class PostgreSQLJDBC {
 
-    public static void main(String args[]) {
-    	
-    	//For Step 2
-    	insertIntoDatabase(2680, 58960, 118);
-    	
-    	//For last step (millions)
-    	//insertIntoDatabase(5000000, 110000000, 250000);
-        
-    }
-    
+	public static void main(String args[]) {
+
+		// For Step 2
+		//insertIntoDatabase(2680, 58960, 118);
+
+		// For last step (less millions)
+		//insertIntoDatabase(500000, 10000000, 25000);
+
+		// For last step (millions)
+		//insertIntoDatabase(5000000, 110000000, 250000);
+
+	}
+
 	public static void testConnection() {
 		Connection c = null;
 		try {
@@ -29,36 +32,32 @@ public class PostgreSQLJDBC {
 		System.out.println("Opened database successfully");
 	}
 
-
-    /**
-     * Step 2:
-	 * Write a program that populates the database with 2,680 matches and 58,960
-	 * players. We are specifying an exact number of tuples in each table for
+	/**
+	 * Step 2: Write a program that populates the database with 2,680 matches and
+	 * 58,960 players. We are specifying an exact number of tuples in each table for
 	 * consistency and usage in different parts in the assignment. You must insert
 	 * players with name that contain the word “pele” 118 times.
 	 * 
-	 * Step Last:
-	 * 5 Mn matches, 110 Mn players
-	 * Write a program that populates an empty database with the number of rows above.
-	 * We are specifying an exact number of tuples in each table for consistency and usage in different
-	 * parts in the assignment. You must insert players with name that contain the word “pele” 250,000
-	 * times.
+	 * Step Last: 5 Mn matches, 110 Mn players Write a program that populates an
+	 * empty database with the number of rows above. We are specifying an exact
+	 * number of tuples in each table for consistency and usage in different parts
+	 * in the assignment. You must insert players with name that contain the word
+	 * “pele” 250,000 times.
 	 *
-	 *	
+	 * 
 	 *
-	 * @params
-	 * numOfMatches : total number of matches to be inserted.
-	 * numOfPlayers : total number of players to be inserted.
-	 * numOfPele 	: number of players named 'Pele' to be inserted.
+	 * @params numOfMatches : total number of matches to be inserted. numOfPlayers :
+	 *         total number of players to be inserted. numOfPele : number of players
+	 *         named 'Pele' to be inserted.
 	 * 
 	 * 
 	 */
 
 	public static void insertIntoDatabase(int numOfMatches, int numOfPlayers, int numOfPele) {
 
-		int numOfPlayersNamedPeleToBeInserted = numOfPele;//118;
-		int numOfMatchesToInsert = numOfMatches;//2680;
-		int numOfPlayersToInsert = numOfPlayers;//58960;
+		int numOfPlayersNamedPeleToBeInserted = numOfPele;// 118;
+		int numOfMatchesToInsert = numOfMatches;// 2680;
+		int numOfPlayersToInsert = numOfPlayers;// 58960;
 		int numOfPlayersPerMatch = numOfPlayersToInsert / numOfMatchesToInsert;
 
 		int m_mid, m_year, m_num_ratings;
@@ -71,7 +70,7 @@ public class PostgreSQLJDBC {
 		Statement stmt = null;
 		try {
 			Class.forName("org.postgresql.Driver");
-			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/DB2Project2", "postgres", "010DB2");
+			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/DB2Project2Large", "postgres", "010DB2");
 			c.setAutoCommit(false);
 			System.out.println("Opened database successfully");
 
@@ -89,7 +88,7 @@ public class PostgreSQLJDBC {
 				sql = "INSERT INTO cup_matches (mid, round, year, num_ratings, rating) " + String
 						.format("VALUES (%d, '%s', %d, %d, %f );", m_mid, m_round, m_year, m_num_ratings, m_rating);
 				stmt.executeUpdate(sql);
-				if(matchesCreated % 1000 == 0 )
+				if (matchesCreated % 1000 == 0)
 					System.out.println("Until match " + matchesCreated + " added!");
 				// Insert a batch of players
 				for (int playersCreated = 1; playersCreated <= numOfPlayersPerMatch; playersCreated++) {
@@ -126,45 +125,54 @@ public class PostgreSQLJDBC {
 		}
 		System.out.println("Records created successfully");
 	}
-	
-	
-    private static String generateRound() {
-        String round = "";
-        int i = (int) (Math.random() * 5 );
-        switch(i) {
-        case 0: round = "32nd"; break;
-        case 1: round = "16th"; break;
-        case 2: round = "quarter_final"; break;
-        case 3: round = "SemiFinal"; break;
-        case 4: round = "Final"; break;
-        }
-        return round;
-    }
-    
-    private static float generateRating() {
-        float i = (float) (Math.random() * 10);
-        return i;
-    }
 
-    private static int generateNumRatings() {
-        int i = (int) (Math.random() * 100000001 );
-        return i;
-    }
+	private static String generateRound() {
+		String round = "";
+		int i = (int) (Math.random() * 5);
+		switch (i) {
+		case 0:
+			round = "32nd";
+			break;
+		case 1:
+			round = "16th";
+			break;
+		case 2:
+			round = "quarter_final";
+			break;
+		case 3:
+			round = "SemiFinal";
+			break;
+		case 4:
+			round = "Final";
+			break;
+		}
+		return round;
+	}
 
-    private static int generateYear() {
-        boolean flag = false;
-        int i = 0;
-        while(!flag) {
-            i = (int) ((Math.random() * 73 ) + 1942);
-            if((i%4) == 0)
-                flag = true;
-        }
-        return i;
-    }
+	private static float generateRating() {
+		float i = (float) (Math.random() * 10);
+		return i;
+	}
+
+	private static int generateNumRatings() {
+		int i = (int) (Math.random() * 100000001);
+		return i;
+	}
+
+	private static int generateYear() {
+		boolean flag = false;
+		int i = 0;
+		while (!flag) {
+			i = (int) ((Math.random() * 73) + 1942);
+			if ((i % 4) == 0)
+				flag = true;
+		}
+		return i;
+	}
 
 	private static int generatePlayerPosition() {
 		int i = (int) (Math.random() * 4) + 1;
-        return i;
+		return i;
 	}
 
 	private static String generatePlayerName() {
